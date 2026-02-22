b32e763482d77ea570d53d10b64feab014ea6173
ennskevin <ennskevin@tutanota.com>
2026-02-22 11:04:01 -0700

    Add <h1> to test deploy action

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -3,6 +3,7 @@ import ProjectEntry from "./ProjectEntry"
 export default function Main() {
     return (
         <>
+            <h1>I AM MAIN</h1>
             <ProjectEntry />
         </>
     )
