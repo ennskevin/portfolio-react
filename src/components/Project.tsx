22b7851b72e31d8b21277dbd3c49c9a0926a9b04
ennskevin <ennskevin@tutanota.com>
2026-03-15 15:44:51 -0600

    Add animation to project handleClick

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -1,10 +1,45 @@
 export default function Project({ name, imgUrl, href }: any) {
+    function handleClick(e: React.MouseEvent<HTMLAnchorElement, MouseEvent>) {
+        e.preventDefault()
+
+        const container = (e.currentTarget.closest(
+            '.project'
+        ) as HTMLElement)
+
+        if (!container) return
+
+        container.style.transform = 'scaleY(0.1)'
+
+        setTimeout(() => {
+            container.style.transform = 'scaleY(0.15) scaleX(0)'
+        }, 100)
+
+        setTimeout(() => {
+            container.style.opacity = '0'
+            window.open(href, '_blank')
+            setTimeout(() => {
+                container.style.transform = ''
+                container.style.opacity = ''
+                container.style.transition = ''
+            }, 50)
+        }, 200)
+
+    }
+
+
     return (
         <section className="project">
-            
-            <a href={href} target="_blank" rel="noopener noreferrer">
-                <img src={imgUrl} alt={name}></img>
-            </a>
+            <div className="project-wrapper">
+                <a 
+                    className="project-link" 
+                    href={href} 
+                    target="_blank" 
+                    rel="noopener noreferrer" 
+                    onClick={handleClick}
+                >
+                    <img src={imgUrl} alt={name}></img>
+                </a>
+            </div>
 
         </section>
     )
